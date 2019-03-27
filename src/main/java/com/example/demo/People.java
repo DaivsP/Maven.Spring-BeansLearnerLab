247c82dd10c1d84921a18955b15502061697a120
Davis <davisperchik@gmail.com>
2019-03-27 19:51:24 -0400

    completed People, probably incorrectly

---
+++ src/main/java/com/example/demo/People.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public abstract class People<PersonType extends Person> {
+    List<PersonType> personList;
+
+    People(List<PersonType> personList){
+        this.personList = personList;
+    }
+
+    People(List<PersonType> personList, Integer people){
+        personList = new ArrayList<>(people);
+    }
+
+    public void add(PersonType person){
+        personList.add(person);
+    }
+
+    public void remove(PersonType person){
+        personList.remove(person);
+    }
+
+    public Integer size(){
+        return personList.size();
+    }
+
+    public void clear(){
+        personList.clear();
+    }
+
+    public void addAll(Iterable<PersonType> personList){
+        this.personList.addAll((Collection<? extends PersonType>) personList);
+    }
+
+    public PersonType findById(long id){
+        for (PersonType person : personList) {
+            if (person.getId() == id){
+                return person;
+            }
+        }
+        return null;
+    }
+
+    public void findAll(){
+        personList.stream()
+        .map(personType -> personList)
+        .forEach(System.out::println);
+    }
+}
